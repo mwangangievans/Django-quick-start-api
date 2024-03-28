4c4ed4e00e4bfc5337dc8e8b42b412724bc4c390
emwangangi <emwangangi@jambopay.com>
2024-03-28 12:06:37 +0300

    rest application

---
+++ quickstart/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.models import Group , User
+from rest_framework import permissions , viewsets
+from quickstart.serializers import UserSerializer ,GroupSerializer
+
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Api endpoint that allows users to be viewed or edited
+    """
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
