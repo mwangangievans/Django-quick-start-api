f342c8ee99985b5c41e946b238de8ea62528d441
emwangangi <emwangangi@jambopay.com>
2024-03-28 12:13:52 +0300

    Groupview view

--- quickstart/views.py
+++ quickstart/views.py
@@ -13,3 +13,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Api endpoint that allows groups to be viewed or edited
+    """   
+
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
